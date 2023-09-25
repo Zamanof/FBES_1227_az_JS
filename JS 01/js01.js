3710628ced879e4c88c79c3714ead3c4983a192a
Zamanof <zamanov@itstep.edu.az>
2023-09-25 21:58:47 +0400

    JS01

---
+++ JS 01/js01.js
@@ -0,0 +1,117 @@
+// console.log("Hello World again")
+// console.log(8*5-2)
+// console.log("5 + 8 =", 5+8)
+
+// console.log(document)
+
+// var, let, const
+
+var name = "Nadir"
+var age = 42
+
+// console.log(name)
+// console.log(age)
+//
+// console.log(typeof (name))
+// console.log(typeof (age))
+
+/*
+Data types
+1. Undefined - bir dəyər qəbul edir undefined
+2. Null - bir dəyər qəbul edir null
+3. Boolean - true, false
+4. Number - 5e-324 - 1.7976931348623157e+308
+    * NaN - not a number
+    * Infinity
+    * -Infinity
+    * -0
+5. String - 'salam', "Hi", `Hello`
+6. Object
+7. Symbol
+*/
+
+/*
+Riyazi operatorlar
++
+-
+*
+/
+%
+**
+++
+--
+
+*/
+// console.log(5 + 3)
+// console.log(5 - 3)
+// console.log(5 * 3)
+// console.log(5 / 3)
+// console.log(1 % 3)
+
+
+// JS coercion
+// console.log('2' + '1')
+// console.log('32' + 2)
+// console.log('32' - 2)
+// console.log('32' - '2')
+// console.log('32' - true)
+// console.log('32' / '6')
+// console.log( true / '20')
+// console.log( true ** '5')
+//
+//
+// var variable = 'Salam'
+//
+// variable++;
+// console.log(variable)
+// var age = "297"
+// alert(typeof (age))
+// console.log('parseInt ->',typeof (Number.parseInt(age)))
+// console.log('Number() ->',typeof (Number(age)))
+// console.log('+ ->',typeof (+age))
+
+// Condition operators
+/*
+>
+<
+>=
+<=
+==
+!=
+===
+!==
+*/
+
+// console.log(3 > '235')
+// console.log('Abdullah' > 'abdullah')
+// console.log('3' == 3)
+// console.log('3' === 3)
+// console.log('3' !== 3)
+// console.log('Abdullah' > 'Ali')
+
+
+/*
+&&
+||
+!
+*/
+
+/*
+&
+|
+^
+<<
+>>
+~
+*/
+
+// console.log(5 & 2)
+// console.log(5 | 2)
+// console.log(65 ^ 356)
+// console.log(293 ^ 356)
+// var numb = 5
+// console.log(~numb + 1)
+// console.log(5 << 3) // 5 * 2^3
+// console.log(40 >> 3) // 40 / 2^3
+// console.log(5 >> 3)
+
